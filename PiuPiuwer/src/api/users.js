a06e88bf7fad58455ec580371f55aee7d2bd4860
Ilton Andrew <iltonandrew@gmail.com>
2020-05-04 18:22:35 -0300

    signup done

--- PiuPiuwer/src/api/users.js
+++ PiuPiuwer/src/api/users.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Alert } from 'react-native';
 
-function hasError(data) {
-    return !Object.keys(data).includes('token');
-}
+// function hasError(data) {
+//     return !Object.keys(data).includes('token');
+// }
 
-export async function signIn(usuario, nome, sobrenome, email, senha, sobre, foto) {
+export async function signIn(usuario, nome, sobrenome, email, senha, sobre, { navigation }) {
+    function navigateToLogin() {
+        navigation.navigate('Login');
+    }
     // console.warn("request: " + nome + usuario + foto)
-
+    if (usuario == '') return Alert.alert('Erro com o Username!', 'O campo não pode estar vazio!')
+    if (nome == '') return Alert.alert('Erro com o Nome!', 'O campo não pode estar vazio!')
+    if (sobrenome == '') return Alert.alert('Erro com o Sobrenome!', 'O campo não pode estar vazio!')
+    if (email == '') return Alert.alert('Erro com o Email!', 'O campo não pode estar vazio!')
+    if (senha == '') return Alert.alert('Erro com a Senha!', 'O campo não pode estar vazio!')
     try {
         let response = await fetch(
             'http://piupiuwer.polijr.com.br/usuarios/',
@@ -36,20 +43,18 @@ export async function signIn(usuario, nome, sobrenome, email, senha, sobre, foto
 
         // Imprime os dados obtidos:
         console.log(data);
-
-        if (!hasError(data)) {
-            // Retorna os dados:
-            return [data, null];
+        // Erro
+        if (!Object.keys(data).includes('id')) {
+            if (Object.keys(data).includes('email')) return Alert.alert('Erro com a Senha!', 'O email deve ser válido \n(example@example.com)!')
+            if (Object.keys(data).includes('username')) return Alert.alert('Erro com o Username!', 'O username já está em uso')
+            return Alert.alert('Oops!', 'Algo está errado!')
         }
-        else {
-            // Retorna o erro:
-            return [null, 'Insira os dados corretamente.'];
-        }
+        Alert.alert('Sucesso!', 'Você agora é um Piuwer!')
+        navigateToLogin()
 
     } catch (error) {
         // Caso haja algum erro, imprima-o e retorne o erro:
         console.error(error);
-        Alert.alert('Sorry, something went wrong.', error.message);
-        return [null, 'Erro de conexão.'];
+        return Alert.alert('Sorry, something went wrong.', error.message);
     }
 }
